4bd249e5856244a0e8f426a5ec340dddca18964b
Jason Judge <jason.judge@consil.co.uk>
2014-03-25 17:08:43 +0000

    Removed some 404 and 500 blogs. Put in email alert if the cache corrupts again Issue #1

--- update_feeds.sh
+++ update_feeds.sh
@@ -9,3 +9,15 @@
 cd /var/www/vhosts/supermondays.org/blogs
 ./planet.py examples/fancy/config.ini
 
+# 2041-03-25 JDJ If the resulting index.html file is empty, then the
+# cache files have probably corrupted (again). Email me to fix (in the
+# future we may just flush the cache fiels automatically, but we'll
+# see how this goes).
+
+if [ -s examples/output/index.html ]
+then
+    # All fine. We have something in the page at least.
+else
+    # Index file is empty or missing.
+    ls -ls examples/output/ | mail -s 'Superblog page is empty! Please fix.' 'jason@academe.co.uk'
+fi
